5f518f933b0574ed801f4116b8e0796813954a9e
Angel D. Munoz <scythes.lol@gmail.com>
2020-05-03 11:17:11 -0600

    improved tiles

--- src/components/bls-tile.ts
+++ src/components/bls-tile.ts
@@ -1,6 +1,19 @@
 import { bindable, BindingMode } from 'aurelia'
+import { OpenProgramEvent } from '../types';
 
 export class BlsTile {
     @bindable({ mode: BindingMode.toView }) title;
+    @bindable({ mode: BindingMode.toView }) programId;
 
+    constructor(private el: Element) { }
+
+    open() {
+        const dispatched = new CustomEvent<OpenProgramEvent>('open-program', {
+            bubbles: true,
+            cancelable: true,
+            composed: true,
+            detail: { name: this.title, programId: this.programId }
+        });
+        this.el.dispatchEvent(dispatched);
+    }
 }
