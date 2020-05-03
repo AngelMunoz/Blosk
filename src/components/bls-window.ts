092da850b91cea5b9742575b6aec871cbac230e4
Angel D. Munoz <scythes.lol@gmail.com>
2020-05-03 12:07:18 -0600

    wc wip

--- src/components/bls-window.ts
+++ src/components/bls-window.ts
@@ -1,4 +1,43 @@
+import { bindable, BindingMode } from 'aurelia';
+import { AppWindow, ProgramType } from '../types';
 
 export class BlsWindow {
+
+    @bindable({ mode: BindingMode.twoWay }) title: string;
+    @bindable({ mode: BindingMode.toView }) window: AppWindow;
+
+    component: HTMLElement;
+
+    constructor(private el: Element) { }
+
+    afterAttach() {
+        if (this.window.program.programType === ProgramType.WebComponent) {
+            const tagExists = this._exists(document.head.querySelectorAll('script'), this.window.program.url)
+            if (!tagExists) {
+                this.appendScriptTag(this.window.program.url);
+            }
+            this.renderComponent();
+        }
+    }
+
+    private _exists(scripts: NodeListOf<HTMLScriptElement>, url: string) {
+        return Array.from(scripts).find(s => s.src === url);
+    }
+
+    beforeDetach() {
+        this.component.remove();
+    }
+
+    appendScriptTag(url: string) {
+        const script = document.createElement('script');
+        script.type = "module";
+        script.src = url;
+        document.head.appendChild(script);
+    }
+
+    renderComponent() {
+        this.component = document.createElement(this.window.program.tag);
+        this.el.appendChild(this.component)
+    }
 
 }
